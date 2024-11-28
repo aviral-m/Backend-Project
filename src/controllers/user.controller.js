64ed50305f39c7740853a6ea9ba6adfc98236d91
aviral-m <aviralmehrotra6@gmail.com>
2024-11-28 13:00:46 +0530

    postman testing + bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,6 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
     const {fullName, email, username, password} = req.body
-    console.log("email: ",email);
 
     //Check if all required fields were filled
     if (
@@ -17,7 +16,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     //check if a user with the username or email already exists
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -27,7 +26,13 @@ const registerUser = asyncHandler( async (req, res) => {
 
     // check for images, make sure avatar is present
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if (req.files && Array.isArray(req.files.coverImage)
+    && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files?.coverImage[0]?.path;
+    }
 
     if (!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,8 +3,8 @@ import fs from "fs"
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: CLOUDINARY_API_KEY, 
-    api_secret: CLOUDINARY_API_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -14,8 +14,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
+        
         //file has been uploaded successfully
-        console.log("File uploaded successfully on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response ;
 
     } catch (error) {
